28e1c7bde11c7dc9f7979399a26107af56783aa4
OneHyun <family8285@gmail.com>
2022-07-21 01:55:28 +0900

    Added routes, components folder

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
-
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  console.log(coins);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `( ${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loding</strong>
-      ) : (
-        <select>
-          {coins.map((coin, index) => (
-            <option key={index}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  return null;
 };
 
 export default App;
