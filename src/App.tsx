933392078fc4b1cbac35a4e3a56e44de4873a9c9
OneHyun <family8285@gmail.com>
2022-08-05 18:53:35 +0900

    Apply TS with style-components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Circle from "./components/Circle";
+
+const App = () => {
+  console.log("test");
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+};
+
+export default App;
