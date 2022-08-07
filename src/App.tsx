cd14d35d1787449cd8c1b8a1fc55b8c06f5364b1
OneHyun <family8285@gmail.com>
2022-08-08 02:09:44 +0900

    Add deco files for Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import Circle from "./components/Circle";
+import React, { useState } from "react";
+import { env } from "process";
+import styled from "styled-components";
 
+const Button = styled.button`
+  background-color: ${(props) => props.theme.btnColor};
+`;
 const App = () => {
-  console.log("test");
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    console.log("Changing", value);
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="check" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <Button>Log in</Button>
+      </form>
     </div>
   );
 };
